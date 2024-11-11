160bdb57f3cf75047a89804f96d535a89f9042c9
CarlosGrijalva06 <c.grijalva0604@gmail.com>
2024-11-11 07:21:50 -0600

    PeTiciones

---
+++ Parcial 3/Peticiones/tempCodeRunnerFile.js
@@ -0,0 +1,25 @@
+const express = require ("express");
+const app = express();
+app.use(express.json());
+app.use(express.text());
+
+
+app.get('/administracion',(req,res)=>{
+    console.log(req.query)
+    res.send('Servidor contestando a peticion query GET')
+});
+
+app.get('/maestros',(req,res)=>{
+    console.log(req.body)
+    res.send('Servidor constestando a peticion body GET')
+});
+
+app.get('/estudiantes/:carrera',(req,res)=>{
+    console.log(req.params.carrera)
+    console.log(req.query.control)
+    res.send('Servidor contestando a peticion params GET')
+});
+
+app.listen(8000,()=>{
+    console.log(`Escuchando en el puerto : ${8000}`)
+});
